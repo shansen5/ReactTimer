559b7ba16517fabbaea7f728c0e876afc98776db
shansen5 <steven.e.hansen@gmail.com>
2017-06-02 23:23:50 -0700

    Add Clock component and tests.

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,11 @@
 var React = require( 'react' );
+var Clock = require( 'Clock' );
 
 var Timer = React.createClass( {
     render: function () {
         return (
             <div>
+                <Clock totalSeconds={256}/>
                 <h1 className="text-center page-title">Timer</h1>
             </div>
         )
