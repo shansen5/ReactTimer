0da6134705e539097a5651d6be213b71ada57104
shansen5 <steven.e.hansen@gmail.com>
2017-06-05 22:00:31 -0700

    Add countdown functionality.

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,16 +5,15 @@ var CountdownForm = React.createClass( {
         e.preventDefault();
         var time = this.refs.time.value;
         if ( time.match( /^[0-9]*$/ )) {
-            this.refs.time.value = '';
+            // this.refs.time.value = '';
             this.props.onSetCountdown( parseInt( time, 10 ));
         } else if ( time.match( /^((?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$)/ )) {
             var hms = time.split(':');
             var seconds = (+hms[0]) * 60 * 60 + (+hms[1]) * 60 + (+hms[2] || 0);
-            this.refs.time.value = '';
+            // this.refs.time.value = '';
             this.props.onSetCountdown( seconds );
         } else {
             this.refs.time.value = '';
-            // this.props.onSetCountdown( 0 );
         }
     },
     render: function () {
